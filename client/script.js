8886da83ad954c74b39e5e104d5e378527263495
ObianuoObi <117560960+ObianuoObi@users.noreply.github.com>
2023-06-10 17:18:39 +0100

    client and server constructed

--- client/script.js
+++ client/script.js
@@ -8,52 +8,52 @@ let loadInterval; // we will use this variable to store the interval that will b
 
 // this function will be used to create a new message element which will return three dots ... as the message
 
-function botLoader (element) {
-  element.textContent = ''; // we set the text content of the elemet to an empty string
+function botLoader(element) {
+  element.textContent = ''; // we set the text content of the element to an empty string
 
-  loadInterval = setInterval(()=> {
+  loadInterval = setInterval(() => {
     element.textContent += '.'; // we add a dot to the text content of the element
 
     if (element.textContent === '....') {
       element.textContent = '';
-      }
+    }
   }, 300);
 }
 
 // this function will be used to implement the bot's response and typing functionality
 
-function botResponse (element, text) {
+function botResponse(element, text) {
   let index = 0; // At the start of the function, we set the index variable to 0
 
-  let interval = setInterval(()=> {
-    if (index < text.length) { // this condition checks if the index is less than the length of the text which means we are still typing
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      // this condition checks if the index is less than the length of the text which means we are still typing
       element.innerHTML += text.charAt(index); // this is going to get the character at the index position and add it to the innerHTML of the element
       index++; // we increment the index by 1
-
-} else {
-  clearInterval(interval); // we clear the interval when the index is equal to the length of the text
-}
-  }, 20)
-
+    } else {
+      clearInterval(interval); // we clear the interval when the index is equal to the length of the text
+    }
+  }, 20);
 }
 
 // this function will be used to generate a unique id for every single message to be able to map through them
 
-function generateUniqueId  () { // current time and date are used to generat unique id's
+function generateUniqueId() {
+  // current time and date are used to generate unique ids
   const timestamp = Date.now(); // we get the current time and date
   const randomNumber = Math.random(); // we get a random number
   const hexadecimalString = randomNumber.toString(16); // we convert the random number to a hexadecimal string
 
   return `id-${timestamp}-${hexadecimalString}`; // we return the unique id
-
 }
 
-function chatStripe (isAi, value,uniqueId) { // this function will be used to generate a new chat stripe
+function chatStripe(isAi, value, uniqueId) {
+  // this function will be used to generate a new chat stripe
   return `
   <div class="wrapper ${isAi ? 'ai' : ''}">
     <div class="chat">
       <div class="profile">
-        <img 
+        <img
           src="${isAi ? bot : user}"
           alt="${isAi ? 'bot' : 'user'}"
         />
@@ -62,25 +62,24 @@ function chatStripe (isAi, value,uniqueId) { // this function will be used to ge
     </div>
   </div>
 `;
-
 }
 
 // create our handle submit function which is going to trigger to get the AI generated response
 
- const handleSubmit =  async (event) => {
-  event.preventDefault(); // we prevent the default behaviour of the form or browser
+const handleSubmit = async (event) => {
+  event.preventDefault(); // we prevent the default behavior of the form or browser
 
   const data = new FormData(form); // To get the data that we typed into the form
 
   // we want to generate a new chat stripe within our HTML for the user's message
-  chatContainer.innerHTML += chatStripe(false, data.get('propmt')); // we set the innerHTML of the chat container to the chat stripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'), generateUniqueId()); // we set the innerHTML of the chat container to the chat stripe
 
   form.reset(); // we reset the form
 
   // we want to generate a new chat stripe within our HTML for the AI's message
 
   const uniqueId = generateUniqueId(); // we generate a unique id for the message
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId); // 
+  chatContainer.innerHTML += chatStripe(true, '', uniqueId);
 
   chatContainer.scrollTop = chatContainer.scrollHeight; // we scroll to the bottom of the chat container
 
@@ -88,12 +87,39 @@ function chatStripe (isAi, value,uniqueId) { // this function will be used to ge
 
   botLoader(messageDiv); // we call the bot loader function
 
-}
+  // fetch data from server
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  clearInterval(loadInterval); // we clear the interval
+  messageDiv.innerHTML = ''; // we set the innerHTML of the message div to an empty string so that we can add the bot's response to it
+
+  if (response.ok) {
+    const responseData = await response.json(); // we get the data from the response
+    const parsedData = responseData.bot.trim(); // we trim the data to remove any white spaces
+
+    console.log({parsedData});
+
+     botResponse(messageDiv, parsedData); 
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Something went wrong';
+
+    alert(err);
+  }
+};
 
 form.addEventListener('submit', handleSubmit); // we add an event listener to the form which will trigger the handle submit function when the form is submitted
 form.addEventListener('keyup', (event) => {
   if (event.keyCode === 13) {
     handleSubmit(event);
   }
-})
-
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-import express from 'express'; 
-import * as dotenv from 'dotenv';
+import express from 'express';
+import dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config(); // Load .env file
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_AI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 }); // Load OpenAI API key from .env file
 
 const openai = new OpenAIApi(configuration); // Create OpenAI API object
@@ -16,33 +16,30 @@ app.use(cors()); // Enable CORS
 app.use(express.json()); // Enable JSON parsing from backend to frontend
 
 app.get('/', async (req, res) => {
-    res.status.send({ 
-        message: 'Hello from Codex'
-    })
-
+  res.status(200).send({ message: 'Hello from Codex' });
 }); // Create GET route
 
-app.post('/', async (res, req) => {
-    try{
-        const prompt = req.body.prompt; // Get prompt from request body
+app.post('/', async (req, res) => {
+  try {
+    const prompt = req.body.prompt; // Get prompt from request body
 
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: `${prompt}`,
-            temperature: 0,
-            max_tokens: 3000,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        }) // Create completion using OpenAI API
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    }); // Create completion using OpenAI API
 
     res.status(200).send({
-        bot: response.data.choices[0].text // Send response from OpenAI API to frontend
-    })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ error }) 
-    }
-}) // Create POST route
+      bot: response.data.choices[0].text, // Send response from OpenAI API to frontend
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+}); // Create POST route
 
 app.listen(5000, () => console.log('Server running on port http://localhost:5000'));
